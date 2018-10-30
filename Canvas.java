cb01a1131aea9f7069fda515578f3155381ba186
programmingflo <flori08mans@gmail.com>
2018-10-30 11:34:01 +0100

    init push

---
+++ Canvas.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * creates a canvas for the game 2048
+ * 
+ * @Florian Mansfeld
+ * @version (a version number or a date)
+ */
+public class Canvas extends World
+{
+
+    /**
+     * Constructor for objects of class Canvas.
+     * 
+     */
+    public Canvas()
+    {
+        super(4, 4, 50); 
+    }
+}
