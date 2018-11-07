c1e86d5e7d43fb173adf3d622da97d3cd8a3c255
programmingflo <flori08mans@gmail.com>
2018-11-07 19:57:22 +0100

    Add Stone class

---
+++ Stone.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Stone on the canvas
+ * 
+ * @author Florian Mansfeld
+ * @version (a version number or a date)
+ */
+public class Stone extends Actor
+{
+    private int value;
+
+    public Stone(){
+        this.value = 2;
+    }
+    public Stone(int value){
+        this.value = value;
+    }
+    public void act() 
+    {
+        // Add your action code here.
+    }
+
+
+}
